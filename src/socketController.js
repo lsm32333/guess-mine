84741120915c0760673d9c09cc3834dae3b4b29e
lsm32333 <lsm32333@naver.com>
2020-03-12 15:34:57 +0900

    #5.0 Refactoring

--- src/socketController.js
+++ src/socketController.js
@@ -3,6 +3,8 @@ import events from "./events";
 const socketController = socket => {
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
+    socket.broadcast.emit(events.newUser, { nickname });
   });
 };
+
 export default socketController;
