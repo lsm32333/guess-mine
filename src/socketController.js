6ee2145aeea106ac2ae6a3e4c28bfad2b71b5d55
lsm32333 <lsm32333@naver.com>
2020-03-12 17:09:22 +0900

    #5.2 Chat part One

--- src/socketController.js
+++ src/socketController.js
@@ -2,7 +2,7 @@ import events from "./events";
 
 const socketController = socket => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
-  
+
   socket.on(events.setNickname, ({ nickname }) => {
     socket.nickname = nickname;
     broadcast(events.newUser, { nickname });
