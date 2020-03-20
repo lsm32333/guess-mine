2c71e50a04f341a965090825b4f5ec50778f9ace
lsm32333 <lsm32333@naver.com>
2020-03-20 17:59:48 +0900

    #6.4 Game Programming part Two

--- src/socketController.js
+++ src/socketController.js
@@ -16,7 +16,12 @@ const socketController = (socket, io) => {
       inProgress = true;
       const leader = chooseLeader();
       word = chooseWord();
+      io.to(leader.id).emit(events.leaderNotif, { word });
+      superBroadcast(events.gameStarted);
     }
+  };
+  const endGame = () => {
+    inProgress = false;
   };
 
   socket.on(events.setNickname, ({ nickname }) => {
@@ -24,10 +29,16 @@ const socketController = (socket, io) => {
     sockets.push({ id: socket.id, points: 0, nickname: nickname });
     broadcast(events.newUser, { nickname });
     sendPlayerUpdate();
+    if (sockets.length === 1) {
+      startGame();
+    }
   });
 
   socket.on(events.disconnect, () => {
     sockets = sockets.filter(aSocket => aSocket.id !== socket.id);
+    if (sockets.length === 1) {
+      endGame();
+    }
     broadcast(events.disconnected, { nickname: socket.nickname });
     sendPlayerUpdate();
     startGame();
